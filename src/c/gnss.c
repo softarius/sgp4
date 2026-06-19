061fda44e3d9724b603cebb39230f838d74b0919
softarius <sergey@yushinin.ru>
2026-06-19 15:27:22 +0300

    Прогноз всех спутников на 120 минут своей эпохи

---
+++ src/c/gnss.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "TLE.h"
+
+typedef struct VERIN
+{
+    char line1[70];
+    char line2[70];
+    double startmin;
+    double stepmin;
+    double stopmin;
+} VERIN;
+
+/**
+ * returns the count of verins read and sets the pointer to an array created with malloc.
+ */
+int readVERINs(VERIN **listptr)
+{
+    char line[256];
+    char *str = NULL;
+    FILE *in_file = NULL;
+    VERIN *verins = NULL;
+    int cnt = 0;
+
+    in_file = fopen("../../data/gnss.tle", "r");
+
+    while (fgets(line, 255, in_file) != NULL)
+    {
+        if (line[0] == '1')
+            cnt++;
+    }
+
+    if (in_file)
+    {
+        fclose(in_file);
+    }
+
+    verins = malloc(cnt * sizeof(VERIN));
+    *listptr = verins;
+
+    cnt = 0;
+    in_file = fopen("../../data/gnss.tle", "r");
+
+    while (fgets(line, 255, in_file) != NULL)
+    {
+        if (line[0] == '1')
+        {
+            strncpy(verins[cnt].line1, line, 69);
+            verins[cnt].line1[69] = 0;
+            fgets(line, 255, in_file);
+            strncpy(verins[cnt].line2, line, 69);
+            verins[cnt].line2[69] = 0;
+            str = &line[70];
+            sscanf(str, "%lf %lf %lf", &verins[cnt].startmin, &verins[cnt].stopmin, &verins[cnt].stepmin);
+
+            cnt++;
+        }
+    }
+
+    if (in_file)
+    {
+        fclose(in_file);
+    }
+
+    return cnt;
+}
+
+void runVER(VERIN *verins, int cnt)
+{
+    TLE tle;
+    double r[3];
+    double v[3];
+    double mins = 120.0;
+    int i;
+
+    for (i = 0; i < cnt; i++)
+    {
+        parseLines(&tle, verins[i].line1, verins[i].line2);
+        getRV(&tle, mins, r, v);
+        printf("%3d %s %ld %18.6f %18.6f %18.6f\n", i + 1, tle.objectID, tle.epoch, r[0], r[1], r[2]);
+    }
+}
+
+int main(void)
+{
+    int cnt = 0;
+    int i = 0;
+    VERIN *verins = NULL;
+
+    cnt = readVERINs(&verins);
+
+    printf("read %d verins\n", cnt);
+    runVER(verins, cnt);
+
+    free(verins);
+    return 0;
+}
